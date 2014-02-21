4397624ba3b511d1c57360783d9702586e8fd10e
Taylor Lapeyre <taylorlapeyre@gmail.com>
2014-02-21 10:54:12 -0600

    add blank rails template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Anything::Application.config.secret_key_base = '606fa3a74777e6b683cc041d2af19ce845bb418477740275b0d1e7b280487daa54f8e90ba72a254e4097102a286288b942bcedbd3909d787490d7b4eed8760fd'
