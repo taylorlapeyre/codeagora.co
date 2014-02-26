882d62bda89037b23dc5ef786610841e7086b6a0
Taylor Lapeyre <taylorlapeyre@gmail.com>
2014-02-25 22:05:08 -0600

    fix up user specs a bit

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,35 +2,31 @@ require 'spec_helper'
 
 describe User do
   before(:each) do
-    @user = User.new username: 'testuser', email: 'test@example.com', password: 'password'
+    @user = User.new username: 'testuser',
+                     email: 'test@example.com',
+                     password: 'password',
+                     password_confirmation: 'password'
   end
 
   it 'is valid with valid attributes' do
     expect(@user).to be_valid
   end
 
-  it 'must have a username' do
-    @user.username = ''
-    expect(@user).to_not be_valid
+  it 'is invalid with invalid attributes' do
+    [:username, :email].each do |attribute|
+      @user[attribute] = nil
+      expect(@user).to_not be_valid
+    end
   end
 
-  it 'must have an email' do
-    @user.email = ''
-    expect(@user).to_not be_valid
-  end
-
-  it 'must have a password' do
-    @user.password = ''
-    expect(@user).to_not be_valid
-  end
-
-  it 'has a unique username' do
+  it 'should have a unique username' do
     new_user = @user.dup
+    new_user.email = "unique@email.com"
     new_user.save
     expect(@user).to_not be_valid
   end
 
-  it 'has a unique username' do
+  it 'should have a unique email' do
     new_user = @user.dup
     new_user.username = 'uniqueusername'
     new_user.save
