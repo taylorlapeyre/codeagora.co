6ede026cf750d035d07c1aaf41033bf0fd1277d0
Taylor Lapeyre <taylorlapeyre@gmail.com>
2014-03-10 19:00:13 -0500

    add snippet routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Agora::Application.routes.draw do
   root 'languages#index'
-  
+
   resources :users,     only: [:create, :destroy]
   resources :sessions,  only: [:create, :destroy]
-  resources :languages, only: [:index, :show]
+  resources :languages, only: [:index, :show], shallow: true do
+    resources :snippets
+  end
 
-  get '/sign-up',  to: "users#new",        as: 'sign_up'
-  get '/sign-in',  to: "sessions#new",     as: 'sign_in'
-  get '/sign-out', to: "sessions#destroy", as: 'sign_out'
+  get '/signup',  to: "users#new",        as: 'sign_up'
+  get '/signin',  to: "sessions#new",     as: 'sign_in'
+  get '/signout', to: "sessions#destroy", as: 'sign_out'
 
   get "/auth/:provider/callback" => "sessions#github_login"
 end
