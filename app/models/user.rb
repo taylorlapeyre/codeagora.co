989fbfd7a67723f6571ee4cf86c8e84dad4db221
Selby Kendrick <selby.kendrick@gmail.com>
2014-03-18 15:20:41 -0500

    Add has_many snippets to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   has_secure_password
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
+  has_many :snippets
+
   validates :username, uniqueness: true, presence: true
   validates :email, uniqueness: true, presence: true, format: { with: VALID_EMAIL_REGEX }
   validates :password, length: { minimum: 6 }
