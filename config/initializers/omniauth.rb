471f6016b63d8bd307710aa9e91de85d5faa7a4a
Taylor Lapeyre <taylorlapeyre@gmail.com>
2014-03-19 10:33:23 -0500

    add new github provider for prod

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  #if Rails.env.production?
-  #  provider :github, '', ''
-  #elsif Rails.env.development?
+  if Rails.env.production?
+    provider :github, '443753a85110d6fc3a85', 'a6db8e865904e05aca2c6ce9dd2df4bbbe390067'
+  elsif Rails.env.development?
     provider :github, '533997e789786066df70', '6cc5033e8e4b6e17539da447e53cc93bdbb64948'
-  #end
-  
+  end
+
   OmniAuth.config.logger = Rails.logger
 end
