e3cb940106addf88b1e402413ce142060c35be3d
Taylor Lapeyre <taylorlapeyre@gmail.com>
2014-03-27 19:46:14 -0500

    tunage

--- app/controllers/languages_controller.rb
+++ app/controllers/languages_controller.rb
@@ -3,7 +3,7 @@ class LanguagesController < ApplicationController
   # Languages are only created and updated through seeds or the console itself.
 
   def index
-    @languages = Language.all
+    @languages = Language.all.order(:name)
   end
 
   def show
