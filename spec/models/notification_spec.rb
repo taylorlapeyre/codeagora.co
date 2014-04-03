6d5567e0b92b83277df8d56d2cd71ed97e1ba7fc
Taylor Lapeyre <taylorlapeyre@gmail.com>
2014-04-03 11:58:32 -0500

    specs on specs on specs

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
 describe Notification do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @commenter = User.create username: 'commenter',
+                     email: 'test@commenter.com',
+                     password: 'password',
+                     password_confirmation: 'password'
+    @owner     = User.create username: 'owner',
+                     email: 'test@ownder.com',
+                     password: 'password',
+                     password_confirmation: 'password'
+    @snippet   = @owner.snippets.new content: 'puts "My Code"'
+  end
+
+  it 'is not valid without content' do
+    notification = @owner.notifications.new(content: '')
+    expect(notification).to_not be_valid
+  end
+
+  it 'gets created every time a user comments on a snippet' do
+    @snippet.save
+    @commenter.comments.create content: 'Awesome', snippet: @snippet
+    expect(@owner.notifications.count).to eq 1
+  end
 end
