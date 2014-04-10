ff12b2dc9494694f66a8d3948d278a99164aa9ac
Selby Kendrick <selby.kendrick@gmail.com>
2014-04-10 00:32:49 -0500

    Function to_training_data in snippet nodel to calculate training data

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -24,6 +24,30 @@ class Snippet < ActiveRecord::Base
     Pygmentize.process(content, language.to_pygments)
   end
 
+  # Calculates training data for a snippet
+  def to_training_data num_letters = 50
+    # Get first n chars and count them by ascii value
+    first_letters = content[0..num_letters]
+    letter_count = Array.new(128, 0.0)
+    first_letters.split("").each do |l|
+      ascii = l.ord
+      if ascii >= 0 && ascii < 128
+        letter_count[ascii] = letter_count[ascii] + 1
+      end
+    end
+
+    # Calculate ascii char frequency by diving by # letters counted
+    letter_freq = letter_count.map { |n| n / first_letters.length }
+
+    # Output training data is a zero array with 1 at the language  id of currecnt snippet
+    num_languages = Language.all.count
+    language = Array.new(num_languages, 0)
+    language[self.language.id-1] = 1
+
+    # Return training data
+    train_data = [letter_freq, language]
+  end
+
   private
 
   def assign_permalink
