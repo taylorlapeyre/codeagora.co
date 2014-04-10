8d98d6daab7dbfeb875b40a8a679f23b4fd5c970
Selby Kendrick <selby.kendrick@gmail.com>
2014-04-10 00:34:39 -0500

    Make the train action accessible at /snippets/train

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Agora::Application.routes.draw do
   root 'snippets#index'
 
-  resources :users,         only: [:create, :destroy, :show]
-  resources :sessions,      only: [:create, :destroy]
-  resources :languages,     only: [:index, :show]
-  resources :comments,      only: [:create]
-  resources :notifications, only: [:update]
-  resources :snippets
+  get '/snippets/train', to: 'snippets#train'
 
   get '/signup',  to: "users#new",        as: 'sign_up'
   get '/signin',  to: "sessions#new",     as: 'sign_in'
   get '/signout', to: "sessions#destroy", as: 'sign_out'
 
   get "/auth/:provider/callback" => "sessions#github_login"
+
+  resources :users,         only: [:create, :destroy, :show]
+  resources :sessions,      only: [:create, :destroy]
+  resources :languages,     only: [:index, :show]
+  resources :comments,      only: [:create]
+  resources :notifications, only: [:update]
+  resources :snippets
 end
