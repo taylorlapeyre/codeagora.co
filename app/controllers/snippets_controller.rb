701f8879faef985cb747b30eab59cabc2337cf75
Selby Kendrick <selby.kendrick@gmail.com>
2014-04-10 00:34:08 -0500

    Create train action in snippets controller and required view

--- app/controllers/snippets_controller.rb
+++ app/controllers/snippets_controller.rb
@@ -7,6 +7,34 @@ class SnippetsController < ApplicationController
     @snippets = Snippet.all
   end
 
+  def train
+    require 'ruby-fann'
+
+    # Collect data for training
+    num_languages = Language.all.count
+    training_data = Snippet.all.map(&:to_training_data)
+
+    # Use inputs as letter frequecy and outputs as desired language
+    inputs = training_data.map { |i| i[0] }
+    ouputs = training_data.map { |i| i[1] }
+
+    # Create the fann train data object
+    train = RubyFann::TrainData.new(:inputs=>inputs, :desired_outputs=>ouputs)
+
+    # Create the artificial neural network
+    fann = RubyFann::Standard.new(:num_inputs=>128, :hidden_neurons=>[64, 32, 8], :num_outputs=>num_languages)
+    
+    # Train the network
+    fann.train_on_data(train, 1000, 10, 0.01) # 1000 max_epochs, 10 errors between reports and 0.01 desired MSE (mean-squared-error)
+    # Test the first snippet.  Why not?
+    outputs = fann.run(Snippet.first.to_training_data[0])
+
+    @hash_out = Hash[outputs.each_with_index.map { |v, i| [i, v] }]
+    @sorted = @hash_out.sort_by {|k,v| v}.reverse
+
+    render :train
+  end
+
   def show
   end
 
